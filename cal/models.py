8a415ba3b68a36a7531552e9bf7ca7d94aa7d44e
name <email@example.com>
2021-03-03 09:51:29 +0000

    datepicker calendar

--- cal/models.py
+++ cal/models.py
@@ -4,10 +4,11 @@ from django.utils import timezone
 
 class CalendarModel(models.Model):
     title = models.CharField(max_length=20)
-    cal_data = models.JSONField(blank=True,null=True)
+    cal_data = models.JSONField("JSONデータ",blank=True,null=True)
     def __str__(self):
         return self.title
 
 class TestModel(models.Model):
     title = models.CharField(max_length=20)
     startDay = models.DateTimeField("開始",default=timezone.now())
+    endDay = models.DateTimeField("終了",default=timezone.now())
--- cal/views.py
+++ cal/views.py
@@ -4,10 +4,13 @@ from django.db import transaction
 from django.db.models import Q
 from django.core.serializers import serialize
 from django.views.generic import TemplateView,FormView
-from .models import CalendarModel
+from .models import CalendarModel,TestModel
 # Create your views here.
 import json
 from datetime import datetime,timezone,timedelta
+
+# タイムゾーンの作成
+JST = timezone(timedelta(hours=9),"JST")
 
 def index(request):
     return render(request,"cal/index.html",{"test":"text"})
@@ -15,45 +18,56 @@ def index(request):
 def get_event(request):
     start = request.GET["start"]
     end = request.GET["end"]
-    print(start)
     # 2021-02-28T00:00:00+09:00
-    print(end)
     # 2021-04-11T00:00:00+09:00
-    # create_time = datetime(2021,3,3,15,0,0).isoformat() + "+09:00"
-    
-    time_format = datetime(2021,3,3,15,0,0,tzinfo=timezone(timedelta(hours=9))).isoformat()
-    # '2021-03-03T15:00:00+09:00'
-    # model save時
-    # model.start = datetime(2021,3,3,15,0,0,tzinfo=timezone(timedelta(hours=9)))
-    print(datetime.fromisoformat(time_format) > datetime.fromisoformat(start))
-    event = [
-        {
-            "title":"経験値3倍",
-            "start":"2021-03-04",
-            "end":"2021-03-04"
-        },
-        {
-            "title":"経験値3倍",
-            "start":"2021-03-08T10:00:00",
-            "end":"2021-03-08T21:00:00"
-        },{
-            "title":"メンテ",
-            "start": "2021-03-10T10:30:00",
-            "end": "2021-03-10T16:00:00"
-        }
-        ]
-    data = CalendarModel.objects.all()
+    print(start,end)
+    start_time = datetime.strptime(start,"%Y-%m-%dT%H:%M:%S%z")
+    end_time = datetime.strptime(end,"%Y-%m-%dT%H:%M:%S%z")
+    print(start_time.isoformat(),end_time.isoformat())
+    # 開始日がstart以上で終了日がend未満
+    datas = TestModel.objects.filter(startDay__gte=start_time,endDay__lt=end_time)
     event = []
-    for k in data:
-        event.append(k.cal_data)
+    for data in datas:
+        eve = {}
+        eve["id"] = data.pk
+        eve["title"] = data.title
+        eve["start"] = data.startDay
+        eve["end"] = data.endDay
+        event.append(eve)
     return JsonResponse(event,safe=False)
 def event_regist(request):
     context = {
-        "foo": "poster"
+        "foo": "登録フォーム"
     }
     return render(request,"cal/post.html",{"context":context})
-
+    
+TimeFormatStyle = "%Y-%m-%d %H:%M"
 def event_set(request):
+    context = {"type":"failed"}
     if request.method == "POST":
-        return HttpResponse("POST")
-    return HttpResponse("GET")
+        title = request.POST["title"]
+        start = request.POST["startDay"]
+        start_time = datetime.strptime(start,TimeFormatStyle).astimezone(JST)
+        end = request.POST["endDay"]
+        end_time = datetime.strptime(end,TimeFormatStyle).astimezone(JST)
+
+        if(title and end_time > start_time):
+            context["type"] = "success"
+            model = TestModel()
+            model.title = title
+            model.startDay = start_time
+            model.endDay = end_time
+            model.save()
+        elif title:
+            context["message"] = "incorrect for date"
+            context["error"] = "date"
+        elif end_time > start_time:
+            context["message"] = "incorrect for title"
+            context["error"] = "title"
+        else:
+            context["message"] = "incorrect for all"
+            context["error"] = "all"
+    else:
+        context["message"] = "incorrect for method"
+        context["error"] = "method"
+    return render(request,"cal/index.html",{"context":context})
